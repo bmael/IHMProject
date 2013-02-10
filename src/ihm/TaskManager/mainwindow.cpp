a9889352ca6fc2b0ddb906d01ae6d672c21ba67c
Thanatos21 <julbiz21@gmail.com>
2013-02-10 23:54:09 +0100

    Works better with the main window...

--- src/ihm/TaskManager/mainwindow.cpp
+++ src/ihm/TaskManager/mainwindow.cpp
@@ -1,5 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGroupBox>
+#include <QDebug>
+
+#include "../../Concept/Manager/task.h"
+#include "../../Concept/Manager/taskcomponent.h"
+#include "../../Concept/Manager/tasklist.h"
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
