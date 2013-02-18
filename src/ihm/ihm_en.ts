401ccd80e2996a37d5539d48ef54387a516422cb
bmael <maelbar44@gmail.com>
2013-02-18 02:07:42 +0100

    try with these files

---
+++ src/ihm/ihm_en.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message utf8="true">
+        <location filename="CustomWidgets/aboutdialog.ui" line="14"/>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+    <message utf8="true">
+        <location filename="CustomWidgets/aboutdialog.ui" line="20"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Moustache&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Un gestionnaire de tâche réalisé en C++/Qt. &lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Développé par&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	Julien BIZEUL&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	Maël BARBIN&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Moustache&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;A task manager written in C++/Qt. &lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Developed by&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	Julien BIZEUL&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	Maël BARBIN&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ListForm</name>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="40"/>
+        <source>Type (facultatif)</source>
+        <translation>Type (optional)</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="50"/>
+        <source>Nom</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="60"/>
+        <source>Date limite</source>
+        <translation>End date</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="67"/>
+        <source>Fixe</source>
+        <translation>Absolute</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="77"/>
+        <source>Relative</source>
+        <translation>Relative</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="103"/>
+        <source>Jours</source>
+        <translation>Days</translation>
+    </message>
+    <message utf8="true">
+        <location filename="CustomWidgets/listform.ui" line="115"/>
+        <source>Priorité</source>
+        <translation>Priority</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/listform.ui" line="125"/>
+        <source>Sous-liste de</source>
+        <translation>Sublist of</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="20"/>
+        <source>Moustache</source>
+        <translation>Moustache</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="55"/>
+        <source>Listes</source>
+        <translation>Lists</translation>
+    </message>
+    <message utf8="true">
+        <location filename="TaskManager/mainwindow.ui" line="68"/>
+        <source>Tâches</source>
+        <translation>Tasks</translation>
+    </message>
+    <message utf8="true">
+        <location filename="TaskManager/mainwindow.ui" line="81"/>
+        <source>Détails</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="106"/>
+        <source>&amp;Fichier</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="118"/>
+        <source>&amp;Aide</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message utf8="true">
+        <location filename="TaskManager/mainwindow.ui" line="124"/>
+        <source>&amp;Édition</source>
+        <translation>&amp;Edit</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="134"/>
+        <source>Quitter</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="137"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="142"/>
+        <source>Nouveau</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="145"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="150"/>
+        <source>Enregistrer</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="153"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="158"/>
+        <source>Enregistrer sous...</source>
+        <translation>Save as...</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="161"/>
+        <source>Ctrl+Shift+S</source>
+        <translation>Ctrl+Shift+S</translation>
+    </message>
+    <message utf8="true">
+        <location filename="TaskManager/mainwindow.ui" line="166"/>
+        <source>À propos de Moustache...</source>
+        <translation>About Moustache...</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="171"/>
+        <source>Ouvrir</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="TaskManager/mainwindow.ui" line="174"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message utf8="true">
+        <location filename="TaskManager/mainwindow.ui" line="179"/>
+        <source>Préférences...</source>
+        <translation>Preferencies...</translation>
+    </message>
+</context>
+<context>
+    <name>Preferencies</name>
+    <message utf8="true">
+        <location filename="CustomWidgets/preferencies.ui" line="14"/>
+        <source>Préférences</source>
+        <translation>Preferencies</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/preferencies.ui" line="20"/>
+        <source>Langue</source>
+        <translation>Language</translation>
+    </message>
+    <message utf8="true">
+        <location filename="CustomWidgets/preferencies.ui" line="26"/>
+        <source>Français</source>
+        <translation>French</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/preferencies.ui" line="36"/>
+        <source>Anglais</source>
+        <translation>Engish</translation>
+    </message>
+</context>
+<context>
+    <name>TaskForm</name>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="34"/>
+        <source>Nom</source>
+        <translation>Name</translation>
+    </message>
+    <message utf8="true">
+        <location filename="CustomWidgets/taskform.ui" line="44"/>
+        <source>Sous-tâche de</source>
+        <translation>Subtask of</translation>
+    </message>
+    <message utf8="true">
+        <location filename="CustomWidgets/taskform.ui" line="54"/>
+        <source>Priorité</source>
+        <translation>Priority</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="64"/>
+        <source>Date limite</source>
+        <translation>End date</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="71"/>
+        <source>Fixe</source>
+        <translation>Absolute</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="81"/>
+        <source>Relative</source>
+        <translation>Relative</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskform.ui" line="107"/>
+        <source>Jours</source>
+        <translation>Days</translation>
+    </message>
+</context>
+<context>
+    <name>TaskListWidget</name>
+    <message>
+        <location filename="CustomWidgets/tasklistwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/tasklistwidget.ui" line="28"/>
+        <source>Nouveau</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/tasklistwidget.ui" line="35"/>
+        <source>Modifier</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/tasklistwidget.ui" line="42"/>
+        <source>Supprimer</source>
+        <translation>Delete</translation>
+    </message>
+</context>
+<context>
+    <name>TaskWidget</name>
+    <message>
+        <location filename="CustomWidgets/taskwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskwidget.ui" line="22"/>
+        <source>Nouveau</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskwidget.ui" line="29"/>
+        <source>Modifier</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="CustomWidgets/taskwidget.ui" line="36"/>
+        <source>Supprimer</source>
+        <translation>Delete</translation>
+    </message>
+</context>
+</TS>
