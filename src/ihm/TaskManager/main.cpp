8cb6dc53b366562465493002066b8308015ab815
Thanatos21 <julbiz21@gmail.com>
2013-02-18 23:49:09 +0100

    Adding the QTextCodecs

--- src/ihm/TaskManager/main.cpp
+++ src/ihm/TaskManager/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QTranslator>
+#include <QTextCodec>
 #include <../ihm/TaskManager/mainwindow.h>
 #include "../Tools/loadconfiguration.h"
 #include "../Tools/configuration.h"
@@ -10,12 +11,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    #if QT_VERSION < 0x050000
+        QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+        QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+    #endif
 
     readConfig("conf/session.conf");
 
     MainWindow w((LangType)Configuration::getInstance()->getLanguage());
     w.show();
-    
+
 
     return a.exec();
 }
